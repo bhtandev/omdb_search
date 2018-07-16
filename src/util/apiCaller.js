458be4b9c7b6a818fef37f5fe638b075b2c004d6
Boon Hui Tan <bhtan.dev@gmail.com>
2018-07-16 14:10:59 +1000

    Made API call dynamic depending on caller. http vs https.

--- src/util/apiCaller.js
+++ src/util/apiCaller.js
@@ -1,7 +1,8 @@
-const API_URL = 'https://www.omdbapi.com/?i=tt3896198&apikey=8c81f9c';
+const API_URL = 'www.omdbapi.com/?i=tt3896198&apikey=8c81f9c';
 
 export default function callApi(queryString, method = 'get', body, options) {
-    return fetch(`${API_URL}${queryString}`)
+    let apiUrl = (location.protocol != 'https:')? 'http://' + API_URL :  'https://' + API_URL;
+    return fetch(`${apiUrl}${queryString}`)
     .then(response => response.json().then(json => ({ json, response })))
     .then(({ json, response }) => {
         if (!response.ok) {
